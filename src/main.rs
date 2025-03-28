4ef37b33222e9cf755b971be9c90b3144625e60c
rob <roby.pakalns@gmail.com>
2025-03-28 20:29:38 +0200

    error handling, json

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,26 @@
 use discord_rich_presence::{activity, DiscordIpc, DiscordIpcClient};
-use dotenv::dotenv;
-use std::env;
-use std::thread::sleep;
-use std::time::{Duration, SystemTime, UNIX_EPOCH};
+use serde::Deserialize;
+use std::{
+    fs::File,
+    io::Read,
+    process::exit,
+    thread::sleep,
+    time::{Duration, SystemTime, UNIX_EPOCH},
+};
+
+#[derive(Deserialize)]
+struct Button {
+    label: String,
+    url: String,
+}
+
+#[derive(Deserialize)]
+struct Config {
+    client_id: String,
+    state: String,
+    details: String,
+    buttons: Vec<Button>,
+}
 
 fn main() {
     let timestamp = SystemTime::now()
@@ -10,26 +28,63 @@ fn main() {
         .unwrap()
         .as_secs() as i64;
 
-    dotenv().ok();
+    let mut file = match File::open("config.json") {
+        Ok(file) => file,
+        Err(_) => {
+            eprintln!("Failed to open config.json file");
+            exit(0);
+        }
+    };
 
-    let client_id = env::var("CLIENT_ID").expect("CLIENT_ID not set in .env file");
+    let mut json_data = String::new();
+    if let Err(_) = file.read_to_string(&mut json_data) {
+        eprintln!("Failed to read config.json file");
+        exit(0);
+    }
 
-    let mut client = DiscordIpcClient::new(&client_id).unwrap();
-    client.connect().unwrap();
+    let config: Config = match serde_json::from_str(&json_data) {
+        Ok(config) => config,
+        Err(_) => {
+            eprintln!("Failed to parse config.json file");
+            exit(0);
+        }
+    };
+
+    let mut client = match DiscordIpcClient::new(&config.client_id) {
+        Ok(client) => client,
+        Err(error) => {
+            eprintln!("Failed to create Discord IPC client: {}", error);
+            exit(0);
+        }
+    };
+
+    if let Err(error) = client.connect() {
+        eprintln!("Failed to connect to Discord IPC: {}", error);
+        exit(0);
+    }
 
     let activity = activity::Activity::new()
-        .state("Developing")
-        .details("Idling")
-        .timestamps(activity::Timestamps::new().start(timestamp))
-        .buttons(vec![
-            activity::Button::new("Tricko.pro", "https://tricko.pro"),
-            activity::Button::new("Community Server", "https://discord.gg/yPjrUrvSzv"),
-        ]);
+        .state(&config.state)
+        .details(&config.details)
+        .buttons(
+            config
+                .buttons
+                .iter()
+                .map(|b| activity::Button::new(&b.label, &b.url))
+                .collect(),
+        )
+        .timestamps(activity::Timestamps::new().start(timestamp));
 
-    client.set_activity(activity.clone()).unwrap();
+    if let Err(e) = client.set_activity(activity.clone()) {
+        eprintln!("Failed to set activity: {}", e);
+        exit(0);
+    }
 
     loop {
         sleep(Duration::from_secs(15));
-        client.set_activity(activity.clone()).unwrap();
+        if let Err(e) = client.set_activity(activity.clone()) {
+            eprintln!("Failed to set activity: {}", e);
+            exit(0);
+        }
     }
 }
